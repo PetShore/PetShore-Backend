aa645ff1b282b0e105741f9d7f6a095e9d1f4bdb
Juan Carlos Alayon Molina <jkam86425@gmail.com>
2022-03-08 15:49:09 -0500

    Proyecto Base

---
+++ src/main/java/edu/eci/ieti/petshore/PetshoreApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.petshore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/petshore/PetshoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.petshore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
