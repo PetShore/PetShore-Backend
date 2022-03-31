a64c026656899cc43003afac6c48772e3a5fa20e
Juan <Juan_munozd@hotmail.es>
2022-03-30 19:20:56 -0500

    CambioTest

--- src/test/java/edu/eci/ieti/petshore/PetshoreApplicationTests.java
+++ src/test/java/edu/eci/ieti/petshore/PetshoreApplicationTests.java
@@ -1,13 +1,43 @@
 package edu.eci.ieti.petshore;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PetshoreApplicationTests {
+public class PetshoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    public PetshoreApplicationTests() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class IntroReactApplication.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        //String[] args = null;
+        //IntroReactApplication.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        assertTrue(true);
+    }
 
 }
